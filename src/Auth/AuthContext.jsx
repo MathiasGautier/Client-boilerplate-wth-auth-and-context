6a599fbe2e62d183419a7392a75f9be316c86b6b
Mathias Gautier <mths.gautier@gmail.com>
2020-07-01 18:28:19 +0200

    added loading when waiting for user context

--- src/Auth/AuthContext.jsx
+++ src/Auth/AuthContext.jsx
@@ -6,20 +6,26 @@ export const AuthContext = createContext();
 export default ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isloaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     apiHandler.isAuthenticated().then((data) => {
       setUser(data.user);
       setIsAuthenticated(data.isAuthenticated);
+      setIsLoaded(true);
     });
   }, []);
 
   return (
     <div>
-        <AuthContext.Provider
-          value={{ user, setUser, isAuthenticated, setIsAuthenticated }}>
-          {children}
-        </AuthContext.Provider>
-    </div>
+    {!isloaded ? (
+      <h1>Loading</h1>
+    ) : (
+      <AuthContext.Provider
+        value={{ user, setUser, isAuthenticated, setIsAuthenticated }}>
+        {children}
+      </AuthContext.Provider>
+    )}
+  </div>
   );
 };
